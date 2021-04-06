cf184cbe07033e1a0be09aa28c88c653b97fc813
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-04-06 13:46:01 +0100

    displayed todo list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
+from todo_app.data import session_items
+
 from flask import Flask, render_template
 
 from todo_app.flask_config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +11,8 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    itemList = session_items.get_items()
+    return render_template('index.html', itemList = itemList)
 
 
 if __name__ == '__main__':
