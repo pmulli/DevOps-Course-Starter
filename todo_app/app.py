5219046a3017ea003f5c0aa0a46fe765b375797b
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-04-06 15:21:08 +0100

    add new item to list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
-from todo_app.data import session_items
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
+
+from todo_app.data import session_items
 
 
 app = Flask(__name__)
@@ -13,6 +13,14 @@ app.config.from_object(Config)
 def index():
     itemList = session_items.get_items()
     return render_template('index.html', itemList = itemList)
+
+ 
+@app.route('/items', methods=['POST'])
+def add_item():
+    title = request.form.get('title')
+    session_items.add_item(title)
+    return render_template('confirmation.html')
+
 
 
 if __name__ == '__main__':
