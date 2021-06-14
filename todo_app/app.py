604a71cc05b9ce9ce712d32305dd618a272e6ce1
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-06-14 09:54:03 +0100

    Added ViewModel

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,12 +9,26 @@ app.config.from_object(Config)
 
 trello_board_id = "609542268e084d62bd913af7"
 
+class ViewModel:
+    def __init__(self, items, lists):
+        self._items = items
+        self._lists = lists
+ 
+    @property
+    def items(self):
+        return self._items
+
+    @property
+    def lists(self):
+        return self._lists
+
 @app.route('/')
 def index():
     trello_board = TrelloBoard(trello_board_id)
-    item_list = trello_board.get_cards()
-    list_list = trello_board.get_lists()
-    return render_template('index.html', itemList = item_list, listList = list_list)
+    items = trello_board.get_cards()
+    lists = trello_board.get_lists()
+    item_view_model = ViewModel(items,lists)
+    return render_template('index.html', view_model=item_view_model)
 
  
 @app.route('/items', methods=['POST'])
@@ -24,9 +38,10 @@ def add_item():
     trello_board = TrelloBoard(trello_board_id)
     trello_board.add_card(title, list_id)
 
-    item_list = trello_board.get_cards()
-    list_list = trello_board.get_lists()
-    return render_template('index.html', itemList = item_list, listList = list_list)
+    items = trello_board.get_cards()
+    lists = trello_board.get_lists()
+    item_view_model = ViewModel(items,lists)
+    return render_template('index.html', view_model=item_view_model)
 
 @app.route('/items/<item_id>')
 def update_item_status(item_id):    
@@ -34,9 +49,10 @@ def update_item_status(item_id):
     trello_board = TrelloBoard(trello_board_id)
     trello_board.update_card_status(item_id, list_id)
 
-    item_list = trello_board.get_cards()
-    list_list = trello_board.get_lists()
-    return render_template('index.html', itemList = item_list, listList = list_list)
+    items = trello_board.get_cards()
+    lists = trello_board.get_lists()
+    item_view_model = ViewModel(items,lists)
+    return render_template('index.html', view_model=item_view_model)
 
 
 if __name__ == '__main__':
