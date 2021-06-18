a91e3c23400c0d5f34ec9a5f98f46879ff5b9791
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-06-18 11:25:18 +0100

    Integration test now working

--- todo_app/tests/test_integration.py
+++ todo_app/tests/test_integration.py
@@ -4,41 +4,43 @@ from unittest.mock import Mock
 
 import pytest
 import dotenv
+import os
 
-class TestIntegration:
+sample_trello_cards_response = [{'id': '60c71900c47a8259cb2c912d', 'idList': '609542268e084d62bd913af8','name': 'Test Card'}]
+sample_trello_lists_response = [{'id': '609542268e084d62bd913af8', 'name': 'Doing'}]
 
-    trello_board_id = "609542268e084d62bd913af7"
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = dotenv.find_dotenv('.env.test')
+    dotenv.load_dotenv(file_path, override=True)
 
-    sample_trello_lists_response = [{'id': '609542268e084d62bd913af8', 'name': 'To Do'}]
+    # Create the new app.
+    test_app = app.create_app()
 
-    @pytest.fixture
-    def client(url):
-        # Use our test integration config instead of the 'real' version
-        file_path = dotenv.find_dotenv('.env.test')
-        dotenv.load_dotenv(file_path, override=True)
- 
-        # Create the new app.
-        test_app = app.create_app()
- 
-        # Use the app to create a test_client that can be used in our tests.
-        with test_app.test_client() as client:
-            yield client
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
 
-    @patch('requests.get')
-    def test_index_page(mock_get_requests, client):
-        # Replace call to requests.get(url) with our own function
-        mock_get_requests.side_effect = TestIntegration.mock_get_lists(f'https://api.trello.com/1/boards/{TestIntegration.trello_board_id}/lists', None)
-        response = client.get('/')
-        #assert response[0]['id'] == '609542268e084d62bd913af8'
-        assert mock_get_requests.side_effect.json.return_value[0]['id'] == '609542268e084d62bd913af8'
+@patch('requests.get')
+def test_index_page(mock_get_requests, client):
+    # Replace call to requests.get(url) with our own function
+    mock_get_requests.side_effect = mock_get
+    response = client.get('/')
+    assert b"Test Card" in response.data
+    
+def mock_get(url, params):
+    if url == f'https://api.trello.com/1/boards/'+os.environ['TRELLO_BOARD_ID']+'/cards':
+        response = Mock()
         
-    def mock_get_lists(url, params):
-        if url == f'https://api.trello.com/1/boards/{TestIntegration.trello_board_id}/lists':
-            response = Mock()
-            
-            # sample_trello_lists_response should point to some test response data
-            response.json.return_value = TestIntegration.sample_trello_lists_response
-            return response
-        return None
+        # sample_trello_lists_response should point to some test response data
+        response.json.return_value = sample_trello_cards_response
+        return response
+    elif url == f'https://api.trello.com/1/boards/'+os.environ['TRELLO_BOARD_ID']+'/lists':
+        response = Mock()
+        
+        # sample_trello_lists_response should point to some test response data
+        response.json.return_value = sample_trello_lists_response
+        return response
+    return None
 
- 
