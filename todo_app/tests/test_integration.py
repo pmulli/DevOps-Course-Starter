50a774a18c71d7d4482bf0945c4a85867e5d0847
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-11-30 15:28:03 +0000

    Integration test not mocking mongodb. It's connecting to real db

--- todo_app/tests/test_integration.py
+++ todo_app/tests/test_integration.py
@@ -1,23 +1,14 @@
 from todo_app import app
-from todo_app.data.todo_board import ToDoBoard
-from unittest.mock import patch
-from unittest.mock import Mock
 
 import pytest
 import dotenv
-import os
 import mongomock
-
-sample_todo_cards_response = [{'_id': '60c71900c47a8259cb2c912d', 'boardId': 'abcde','status': 'Doing','name': 'Test Card'}]
 
 @pytest.fixture
 def client():
     # Use our test integration config instead of the 'real' version
     file_path = dotenv.find_dotenv('.env.test')
     dotenv.load_dotenv(file_path, override=True)
-
-    # Create the new app.
-    test_app = app.create_app()
 
     # Use the app to create a test_client that can be used in our tests.
     with mongomock.patch(servers=(('fakemongo.com', 27017),)):
@@ -27,13 +18,8 @@ def client():
 
 
 def test_index_page(client):
-    todo_board = ToDoBoard("fake")
-    todo_board.create_card("Shopping", "To Do")
-
-    cards = todo_board.get_cards()
-
+    client.post('/items', data=dict(title='Test Card', status='To Do'))
     response = client.get('/')
-    #assert b"Test Card" in response.data
-    assert True
+    assert b"Test Card" in response.data
     
 
