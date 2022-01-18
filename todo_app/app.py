b75d2026c6b0acf8484f08bf56a0ef8bfc327667
Paul Mullineux <paul.d.mullineux@aexp.com>
2022-01-18 17:07:28 +0000

    auth token coming back

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,14 +6,44 @@ from todo_app.data.todo_board import ToDoBoard
 
 import os
 
+from flask_login import LoginManager, login_required
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import sys
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('todo_app.flask_config.Config')
 
     todo_board_id = os.getenv('TODO_BOARD_ID')
+    github_client_id = os.getenv('CLIENT_ID')
+    github_client_secret = os.getenv('CLIENT_SECRET')
 
+    login_manager = LoginManager()
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        client = WebApplicationClient(github_client_id)
+        return redirect(client.prepare_request_uri('https://github.com/login/oauth/authorize'))
+        
+    @login_manager.user_loader
+    def load_user(user_id):
+        return None
+
+    login_manager.init_app(app) 
+
+    @app.route('/login/callback')
+    def login():
+        request_code = request.args.get('code')
+        client = WebApplicationClient(github_client_id)
+        data={'client_id': github_client_id, 'client_secret': github_client_secret, 'code': request_code}
+        resp = requests.post('https://github.com/login/oauth/access_token', data)
+        print(resp.text, file=sys.stdout)
+        #client.parse_request_body_response(resp)
+        return redirect('/error')
 
     @app.route('/')
+    @login_required
     def index():
         todo_board = ToDoBoard(todo_board_id)
         items = todo_board.get_cards()
@@ -22,6 +52,7 @@ def create_app():
 
     
     @app.route('/items', methods=['POST'])
+    @login_required
     def add_item():
         title = request.form.get('title')
         status = request.form.get('status')
@@ -33,6 +64,7 @@ def create_app():
         return redirect('/')
 
     @app.route('/items/<item_id>')
+    @login_required
     def update_item_status(item_id):    
         status = request.args.get('status')
         todo_board = ToDoBoard(todo_board_id)
