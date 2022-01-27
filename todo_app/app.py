cfe93568eeaeeee75f3b31a8a8509179314862b0
Paul Mullineux <paul.d.mullineux@aexp.com>
2022-01-27 13:49:33 +0000

    removed redirect parameter from oauth call

--- todo_app/app.py
+++ todo_app/app.py
@@ -45,7 +45,6 @@ def create_app():
         token_url, headers, body = client.prepare_token_request(
             'https://github.com/login/oauth/access_token',
             authorization_response=request.url,
-            redirect_url=request.base_url,
             code=request_code,
             client_secret=github_client_secret
         )
