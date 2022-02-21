226c24c560ccb8c027f4dec5fbd36a0b5512aae6
Paul Mullineux <paul.d.mullineux@aexp.com>
2022-02-21 17:14:37 +0000

    trying to get webhook.sh to execute

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,4 @@
 echo excute azure webhook to trigger restart with latest docker image
 echo $WEBHOOK_URL
 env
-curl -dH -X POST "https://\$pdm-todo:9wcmycL3FDAXCHX6YQKLPBWJZnBAhaEomNSBqlRv7nimnwSDwiMrg1KaRnCw@pdm-todo.scm.azurewebsites.net/docker/hook"
+curl -dH -X POST "$WEBHOOK_URL"
