4e015f93dad0e5dd0e29a286de3b4fbd23cfa73a
Paul Mullineux <paul.d.mullineux@aexp.com>
2022-03-18 16:40:31 +0000

    MongoDB created

--- main.tf
+++ main.tf
@@ -41,5 +41,56 @@ resource "azurerm_app_service" "main" {
   
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "MONGODB_CONNECTION_STRING" = azurerm_cosmosdb_account.db.connection_strings[0]
+  }
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "pdm-todo-cosmos-db"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableServerless"
+  }
+  
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = data.azurerm_resource_group.main.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "todo-db" {
+  name                = "pdm-todo-cosmos-mongo-db"
+  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.db.name
+  lifecycle {
+    prevent_destroy = true
   }
 }
